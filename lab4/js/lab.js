3f27f349f1cb45d2f9b741489b24b03c49d6aac5
sandyy6890 <astrisha@ucsc.edu>
2024-10-17 18:08:28 -0700

    added psuedocode and css

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,18 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Psuedocode for the Cows and Bulls game
+// Author: Sandy Strishak and Panetha Jayakumar
+// Date: October 17, 2024
 
-// Constants
+// A psuedocode for Cows and Bulls Game:
+  // 1. Get the number of letters as input, with any value between 3 and 8.
+  // 2. Get the number of attempts allowed as input, with any value between 8 and 25.
+  // 3. Get the number of player(s) involved in the game as input. 
+  // 4. Generate the random word based on the number of letters input.
+    // 4.1 If one player, the computer is prompted to generate a word with a word length equal to the number of letters input. 
+    // 4.2 If two or more players are involved, prompt to select the player who needs to select or enter a random word. The selected player will now be prompted to select or enter the word of his choice, which should be validated to match the number of letters input.
+  // 5. Get a word guess as input with restricted word length.
+  // 6. Assess the guessed word with the word chosen.
+  // 7. Prompt the player stating the number of cows and bulls.
+  // 8. If 4 bulls, the player who guessed correctly wins.8.1 Generate a prompt to display 'Winner is Player X' and proceed to the step 10. 
+  // 9. Otherwise, steps 5-7 are repeated until the word guessed has 4 bulls. 9.1 If the number of attempts has been reached, generate a prompt 'Player X Lost. No more attempts available. The correct word is XXX' and proceed to step 10.
+  // 10. Generate a Prompt with options for a new game or a new word or to end the game.10.1 If new game is chosen, repeat from step 1.10.2 If new word is chosen, repeat from step 4.10.3 If exit is chosen, close the console.</p>
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
