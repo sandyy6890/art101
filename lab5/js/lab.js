c0de3f73dbb756319daa4b1a3e56e018e9477984
sandyy6890 <astrisha@ucsc.edu>
2024-10-21 15:34:00 -0700

    stylized html

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Lab 5 - Javascript Program
+// Author: Sandy Strishak
+// Date: 21 October
 
-// Constants
 
-// Functions
+// Define Variables
+make = "BMW";
+model = "X1";
+color = "Blue";
+year = 2014;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// Calculate
+age = 2024 - year;
 
-// let's get this party started
-main();
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
+
