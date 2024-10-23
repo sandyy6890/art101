e517fe342b4b2fb6da9769b3409570d3192dc173
sandyy6890 <astrisha@ucsc.edu>
2024-10-23 14:29:00 -0700

    lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Lab 6 - Arrays and Objects
+// Author: Sandy Strishak
+// Date: 10.23.2024
 
-// Constants
+// myTransport array
+myTransport = ["Blue Beamer", "walk", "bus"];
 
-// Functions
+// Object For myMainRide
+myMainRide = {
+      make: "BMW",
+      model: "X1",
+      color: "Blue",
+      year: "2014",
+      age: function() {
+          return 2024 -this.year;
+      }
+    }
+//output
+document.writeln("Kinds of transportation I use: ", myTransport, "<br>");
+document.writeln("My Main Ride: <pre>", 
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
